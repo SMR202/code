0637933058dde6574b9c156860547d6ae6dff34d
SMR202 <127597229+SMR202@users.noreply.github.com>
2023-09-17 22:39:40 +0500

    Add files via upload

--- Q6.cpp
+++ Q6.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
 int main()
 {
+    unsigned int k;
+    
+    cout << "Input a positive integer of 32 bits" << endl;
+    cin >> k;
+
+    bitset <32> gates (k);
+
+    unsigned int constant;
+    unsigned int g1; 
+    unsigned int g2;
+    unsigned int g3;
+    
+    constant=k>>24;
+    g1=k<<24;
+    g1=g1>>24;
+    g2=k<<16;
+    g2=g2>>24;
+    g3=k<<8;
+    g3=g3>>24;
 
 
+    unsigned int key= g1^g2^g3^constant;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    cout<<"Quantum key is: " <<  bitset <8> (key) <<endl;
 
 
 
---
+++ Q7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+
+unsigned short i; //Input i
+
+cout << "Enter a 16 bit integer" << endl;
+cin >> i;
+
+unsigned short grey = i^(i>>1);
+
+
+cout << "The grey code is: " << bitset <16> (grey) << endl;
+
+
+return 0;
+}
